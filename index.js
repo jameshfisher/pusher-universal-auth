513fc03ef30d34a5229ebf80dcd1592200fe0522
James Fisher <jameshfisher@gmail.com>
2016-10-31 13:43:09 +0000

    CORS

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
